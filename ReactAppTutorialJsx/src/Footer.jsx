29cb4f7dbc48bd67a4f00830c1ca38f1ed85d6a3
David N. <nguyen.dddavid@hotmail.com>
2024-06-17 11:46:49 -0400

    First commit

---
+++ ReactAppTutorialJsx/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer() {
+
+    return (
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} My React Website
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
